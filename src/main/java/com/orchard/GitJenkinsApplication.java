fba795846a9e39f8ef767b81ac7aa61e9ba65409
unknown <mindc1july220@iiht.tech>
2022-09-23 13:31:13 +0530

    first commit

---
+++ src/main/java/com/orchard/GitJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.orchard;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(GitJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("app started ........");
+	}
+	
+	
+	public static void main(String[] args) {
+		logger.info("app executed ........");
+		SpringApplication.run(GitJenkinsApplication.class, args);
+	}
+
+}
